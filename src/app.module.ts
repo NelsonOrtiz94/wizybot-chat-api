ec3a7b6cf7959ac811108532ed2f6190d7e0e749
NelsonOrtiz94 <ortnn990@gmail.com>
2025-04-09 09:25:58 -0500

    Adecuaciones

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-// 👉 Importa tus servicios y controladores reales:
 import { ChatbotController } from './chatbot/chatbot.controller';
 import { ChatbotService } from './chatbot/chatbot.service';
 import { ProductSearchService } from './tools/product-search.service';
